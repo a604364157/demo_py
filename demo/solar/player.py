d3a9930a9dcce234c82d16ee6bf1fc8adbf90fb9
jiangjx <640364157@qq.com>
2019-09-02 18:19:49 +0800

    初始化

--- demo/solar/player.py
+++ demo/solar/player.py
@@ -1,5 +1,6 @@
 # 春分，雨水，惊蛰，春分，清明，谷雨，立夏，小满，芒种，夏至，小暑，大暑
 # 立秋，处暑，白露，秋分，寒露，霜降，立冬，小雪，大雪，冬至，小寒，大寒
+import os
 
 from playsound import playsound
 
@@ -8,4 +9,10 @@ def play_mp3(name):
     playsound(name)
 
 
-play_mp3("mp3/芒种.mp3")
+def play_sys(name):
+    # 这个在windows下会调用系统默认音乐播放工具播放
+    os.system(name)
+
+
+play_mp3("mp3/9.mp3")
+# play_sys("mp3\\9.mp3")

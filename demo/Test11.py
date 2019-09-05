7fbfddc58a36a8830bccf67dd32b16542807d2f4
jiangjx <640364157@qq.com>
2019-09-05 09:42:03 +0800

    初始化

--- demo/Test11.py
+++ demo/Test11.py
@@ -1,5 +1,6 @@
 # lambda表达式
 # lambda [parameter_list] : 表达式
+import functools
 
 
 def get_math(t):
@@ -13,3 +14,41 @@ _sum = get_math("s")
 print(_sum(1, 2))
 _sum = get_math("p")
 print(_sum(1, 2))
+
+
+# map()函数
+# map(function, iterable)
+def test1():
+    a = [1, 2, 3, 4, 5]
+    b = map(lambda x: x + 1, a)
+    # map()返回一个map对象，需要转换
+    b = list(b)
+    print(b)
+    # map()也可以传入多个迭代对象
+    c = map(lambda x, y: x + y, a, b)
+    print(list(c))
+
+
+# filter()函数
+# filter(function, iterable)
+def test2():
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    b = filter(lambda x: x % 2 == 0, a)
+    # 同样返回filter对象，需要转一下
+    b = list(b)
+    print(b)
+    # 不能迭代多个对象
+
+
+# reduce()函数
+# reduce(function, iterable)
+def test3():
+    a = [1, 2, 3, 4, 5]
+    # 计算 1*2*3*4*5
+    b = functools.reduce(lambda x, y: x * y, a)
+    print(b)
+
+
+test1()
+test2()
+test3()

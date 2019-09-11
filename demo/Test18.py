db645750ca6f30a0640320ae92c7346217a2e4b6
jiangjx <640364157@qq.com>
2019-09-11 09:34:25 +0800

    初始化

--- demo/Test18.py
+++ demo/Test18.py
@@ -1,9 +1,11 @@
 # 跳过了异常处理和内置函数的讲解
 
 # 本文件讲解常见模块的用法
+import json
 import os
 import random
 import sys
+import time
 
 
 def t1():
@@ -69,7 +71,48 @@ def t3():
     print(random.sample([10, 20, 30, 40, 50], k=4))
 
 
+def t4():
+    # 将当前时间转换为时间字符串
+    print(time.asctime())
+    # 将指定时间转换时间字符串，时间元组的后面3个元素没有设置
+    print(time.asctime((2018, 2, 4, 11, 8, 23, 0, 0, 0)))  # Mon Feb  4 11:08:23 2018
+    # 将以秒数为代表的时间转换为时间字符串
+    print(time.ctime(30))  # Thu Jan  1 08:00:30 1970
+    # 将以秒数为代表的时间转换为struct_time对象。
+    print(time.gmtime(30))
+    # 将当前时间转换为struct_time对象。
+    print(time.gmtime())
+    # 将以秒数为代表的时间转换为代表当前时间的struct_time对象
+    print(time.localtime(30))
+    # 将元组格式的时间转换为秒数代表的时间
+    print(time.mktime((2018, 2, 4, 11, 8, 23, 0, 0, 0)))  # 1517713703.0
+    # 返回性能计数器的值
+    print(time.perf_counter())
+    # 返回当前进程使用CPU的时间
+    print(time.process_time())
+    # time.sleep(10)
+    # 将当前时间转换为指定格式的字符串
+    print(time.strftime('%Y-%m-%d %H:%M:%S'))
+    st = '2018年3月20日'
+    # 将指定时间字符串恢复成struct_time对象。
+    print(time.strptime(st, '%Y年%m月%d日'))
+    # 返回从1970年1970年1月1日0点整到现在过了多少秒。
+    print(time.time())
+    # 返回本地时区的时间偏移，以秒为单位
+    print(time.timezone)  # 在国内东八区输出-28800
+
+
+def t5():
+    # 将py对象转json串
+    s = json.dumps({"a": 1, "b": 2, "c": 3})
+    print(s)
+    s = json.JSONEncoder().encode({"a": 1, "b": 2, "c": 3})
+    print(s)
+
+
 if __name__ == '__main__':
     # t1()
     # t2()
-    t3()
+    # t3()
+    # t4()
+    t5()

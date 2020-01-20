74d68801a9749fc0bb5b31f2e1c48c60f3ea4b51
jiangjx <640364157@qq.com>
2020-01-20 16:48:57 +0800

    初始化

--- demo/flask/server/two_color.py
+++ demo/flask/server/two_color.py
@@ -1,9 +1,14 @@
 import random
 
+reds = [i for i in range(1, 34)]
+blues = [i for i in range(1, 17)]
+
 
 def run_two_color():
-    reds = [i for i in range(1, 34)]
-    blues = [i for i in range(1, 17)]
+    """
+    随机生成一组双色球号码
+    :return: 列表
+    """
     out = random.sample(reds, k=6)
     out.sort()
     return out + random.sample(blues, k=1)
